a76b991702e4b2353b2ce84ba0acdb7daa15265e
mradamgbeck <mradamgbeck@gmail.com>
2020-06-05 12:18:01 -0400

    added component scan for sandboxdependency

--- src/main/java/org/kgrid/sandboxapplication/SandboxApplication.java
+++ src/main/java/org/kgrid/sandboxapplication/SandboxApplication.java
@@ -2,12 +2,13 @@ package org.kgrid.sandboxapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.kgrid.sandboxapplication", "org.kgrid.sandboxdependency"})
 public class SandboxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SandboxApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SandboxApplication.class, args);
+  }
 }
