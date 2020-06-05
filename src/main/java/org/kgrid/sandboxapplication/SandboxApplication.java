089ebd4685f50a86164e07d44b99668a653a3c7e
mradamgbeck <mradamgbeck@gmail.com>
2020-06-05 12:19:53 -0400

    removed component scan

--- src/main/java/org/kgrid/sandboxapplication/SandboxApplication.java
+++ src/main/java/org/kgrid/sandboxapplication/SandboxApplication.java
@@ -2,10 +2,8 @@ package org.kgrid.sandboxapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.kgrid.sandboxapplication", "org.kgrid.sandboxdependency"})
 public class SandboxApplication {
 
   public static void main(String[] args) {
